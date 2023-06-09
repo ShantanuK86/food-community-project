ab3d2fbf40b895859dca0cf4b884ef9657acbd79
ShantanuK86 <sk9411011994@gmail.com>
2023-06-09 10:28:13 +0530

    some changes

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Shantanu's Foodary © 2023</p>
+    </div>
+    );
+  }
+  export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,40 @@
+export const my_recipe = {
+  recipe_1: {
+    name: 'Chirashi Don',
+    preparation_time: '30 minutes',
+    servings: '2',
+    ingredients: [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    instructions: [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+    img: "/img/recipe1.jpg",
+    
+  },
+  recipe_2:{
+    name: 'Veg Sandwich',
+    preparation_time: '15 minutes',
+    servings: '1',
+    ingredients: [
+      '4 slices bread',
+      '2 tsps butter',
+      '2 green chilles',
+      '1 tsps of oli, garlic paste',
+      '1 tomato and pinch of salt',
+    ],
+    instructions: [
+      'Butter  the bread slices on the outer side',
+      'In inner side spread the paste and veggie mix',
+      'cover it with another slice',
+      'grill it in sandwich toaster until golden',
+    ],
+    img: '/img/recipe2.jpg',
+
+  }
+}
